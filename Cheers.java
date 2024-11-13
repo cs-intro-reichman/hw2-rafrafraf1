5d2e1dcb4abc3d073214e60db98f53052ebaa27f
Rafael Asmoucha <rasmoucha@gmail.com>
2024-11-13 17:49:19 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,11 +1,20 @@
 // Prints a crowd cheering output.
+
+import java.util.List;
+
 public class Cheers {
         public static void main(String[] args) {
 	    String word = args[0];
             int number = Integer.parseInt(args[1]);
+            List<Character> nchars = List.of('A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X');
+            String n;
             for (int i = 0; i < word.length(); i++) {
                 char cha = word.charAt(i);
-                System.out.println("Give me an " + cha +": " + cha + "!");
+                n = "  ";
+                if (nchars.contains(Character.toUpperCase(cha))) {
+                        n = "n ";
+                }
+                System.out.println("Give me a" + n + cha +": " + cha + "!");
             }
             System.out.println("What does that spell?");
             for (int x = 0; x < number; x++) {
