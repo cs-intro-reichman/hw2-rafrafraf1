1165db778ee6984db2ae1010bce92176b2ec2de8
Rafael Asmoucha <rasmoucha@gmail.com>
2024-11-13 16:58:16 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,15 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0];
+            int number = Integer.parseInt(args[1]);
+            for (int i = 0; i < word.length(); i++) {
+                char cha = word.charAt(i);
+                System.out.println("Give me an " + cha +": " + cha + "!");
+            }
+            System.out.println("What does that spell?");
+            for (int x = 0; x < number; x++) {
+                System.out.println(word + "!!!");
+            }
         }
 }
